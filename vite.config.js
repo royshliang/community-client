4b9bb3498203ecfc639e2342082c1e8cf741cf36
royliangsehhaw <liangsehhaw@gmail.com>
2023-09-15 11:37:39 +0800

    0.1.1

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         vue(),
 
         VitePWA({
-            version: "0.1.0",
+            version: "0.1.1",
             registerType: 'autoUpdate',
             manifest: {
                 name: 'My Awesome App',
