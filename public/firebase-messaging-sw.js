1e5c5cc728a0105a902333ed2f31b99c3686a5ef
royliangsehhaw <liangsehhaw@gmail.com>
2023-09-15 13:59:38 +0800

    0.1.6

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,23 +30,23 @@ messaging.onBackgroundMessage((payload) => {
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
 // ------- handling notification click action
-// self.addEventListener('notificationclick', event => {
-//     event.notification.close();
-//     event.waitUntil(
-//         clients.matchAll({
-//             includeUncontrolled: true,
-//             type: 'window'
-//         })
-//         .then(function(clientList) {
-//             for(var i=0; i<clientList.length; i++) {
-//                 var client = clientList[i];
-//                 if(client.url == "/" && 'focus' in client) {
-//                     return client.focus();
-//                 }
-//                 if(clients.openWindow) {
-//                     return clients.openWindow('/');
-//                 }
-//             }
-//         })
-//     )
-// });
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({
+            includeUncontrolled: true,
+            type: 'window'
+        })
+        .then(function(clientList) {
+            for(var i=0; i<clientList.length; i++) {
+                var client = clientList[i];
+                if(client.url == "/" && 'focus' in client) {
+                    return client.focus();
+                }
+                if(clients.openWindow) {
+                    return clients.openWindow('/');
+                }
+            }
+        })
+    )
+});
