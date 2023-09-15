16932f668107ea87723d0cf0cfc16b6d802f7fa5
royliangsehhaw <liangsehhaw@gmail.com>
2023-09-15 10:59:35 +0800

    0.0.8

--- dist/firebase-messaging-sw.js
+++ dist/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging.js');
 
 // Initialize Firebase
 var firebaseConfig = {
